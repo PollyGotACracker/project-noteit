729de015eca0c3377b4c058df53d4869d60a6d19
PollyGotACracker <bitterns96@gmail.com>
2023-10-18 02:18:10 +0900

    fix: Home link 클릭 시 flicker 현상 수정

--- client/src/components/homeLink.jsx
+++ client/src/components/homeLink.jsx
@@ -1,10 +1,23 @@
-import { Link } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router";
+import { useRecoilValue } from "recoil";
+import { isSignedInState } from "@recoils/user";
+import { URLS } from "@/router";
 
 const HomeLink = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const isSignedIn = useRecoilValue(isSignedInState);
+
+  const clickHomeHandler = () => {
+    if (isSignedIn && location.pathname === URLS.DASHBOARD) return;
+    if (isSignedIn) navigate(URLS.DASHBOARD);
+    if (!isSignedIn) navigate("/");
+  };
+
   return (
-    <Link className="home-link" to={"/"}>
+    <button className="home-link" onClick={clickHomeHandler} type="button">
       NoteIT
-    </Link>
+    </button>
   );
 };
 
