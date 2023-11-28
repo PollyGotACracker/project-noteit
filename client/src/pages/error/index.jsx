8962e51b75851c7cc8c0869833032ae5b6ad5692
PollyGotACracker <bitterns96@gmail.com>
2023-11-28 21:57:56 +0900

    fix: Error 페이지 레이아웃 조정

--- client/src/pages/error/index.jsx
+++ client/src/pages/error/index.jsx
@@ -1,17 +1,20 @@
 import "@styles/error/error.css";
 import { ReactComponent as NotFoundSvg } from "@assets/images/not_found.svg";
 import { useRecoilValue } from "recoil";
-import { getStyle } from "@utils/manageStyle";
+import useThemeStyle from "@hooks/useThemeStyle";
 import { isSignedInState } from "@recoils/user";
 import { SignInLayout, SignOutLayout } from "@/routeList";
 
-const NotFound = () => {
+const NotFound = ({ isSignedIn }) => {
+  const primary = useThemeStyle("--primary");
   // svg 를 component 형식으로 import 하면 편집 가능
   // svg 파일에서 편집할 색상을 current 로 지정하면
   // 컴포넌트 fill 속성의 값으로 채워짐
   return (
-    <main className="NotFound">
-      <NotFoundSvg className="not-found-img" fill={getStyle("--primary")} />
+    <main className={`NotFound ${!isSignedIn && "out"}`}>
+      <div className="img-wrapper">
+        <NotFoundSvg className="not-found-img" fill={primary} />
+      </div>
       <h1>404 Not Found</h1>
       <p>요청한 URL 페이지를 찾을 수 없습니다.</p>
     </main>
@@ -23,11 +26,11 @@ const ErrorPage = () => {
 
   return isSignedIn ? (
     <SignInLayout>
-      <NotFound />
+      <NotFound isSignedIn={isSignedIn} />
     </SignInLayout>
   ) : (
     <SignOutLayout>
-      <NotFound />
+      <NotFound isSignedIn={isSignedIn} />
     </SignOutLayout>
   );
 };
