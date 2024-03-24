98d188965ff12731a6dd15c79fe282ee124a6259
PollyGotACracker <bitterns96@gmail.com>
2024-03-24 23:56:44 +0900

    fix: 토스트 모바일 위치 오류 수정

--- client/src/components/toast.jsx
+++ client/src/components/toast.jsx
@@ -1,17 +1,31 @@
 import "@styles/components/toast.css";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { RxCross2 } from "react-icons/rx";
 
 const Toast = ({ isShowed, onClose, text }) => {
+  const targetRef = useRef(null);
+  const initialHeight = useRef(window.innerHeight);
+
   useEffect(() => {
     setTimeout(onClose, 8000);
   }, []);
+
+  visualViewport.addEventListener("resize", (e) => {
+    if (!targetRef.current) return;
+    const toast = targetRef.current.parentElement;
+    const keyboardHeight = initialHeight.current - e.target.height;
+    if (keyboardHeight > 0) {
+      toast.style.bottom = `${keyboardHeight}px`;
+    } else {
+      toast.style.bottom = `0`;
+    }
+  });
 
   if (isShowed) {
     return (
       <>
         <p>{text}</p>
-        <button className="close-btn" onClick={onClose}>
+        <button ref={targetRef} className="close-btn" onClick={onClose}>
           <RxCross2 />
         </button>
       </>
